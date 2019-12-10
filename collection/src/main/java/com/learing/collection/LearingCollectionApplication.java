97fbd9d417622ecc66ee387b2182df01d8452941
BIM002 <1030204711@qq.com>
2019-12-10 17:19:43 +0800

    hashMap

---
+++ collection/src/main/java/com/learing/collection/LearingCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.learing.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearingCollectionApplication.class, args);
+    }
+
+}
---
+++ collection/src/test/java/com/learing/collection/LearingCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learing.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearingCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
