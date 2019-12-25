27d80017ea1c1662a67eea22d0be72e045eb5d98
BIM002 <1030204711@qq.com>
2019-12-25 15:09:44 +0800

    更新lock锁分类 mybatis拦截器

--- collection/src/main/java/com/learing/collection/intercept/MybatisPageIntercept.java
+++ collection/src/main/java/com/learing/collection/intercept/MybatisPageIntercept.java
@@ -1,6 +1,7 @@
 package com.learing.collection.intercept;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.binding.MapperMethod;
 import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.executor.statement.RoutingStatementHandler;
 import org.apache.ibatis.executor.statement.StatementHandler;
@@ -37,6 +38,7 @@ public class MybatisPageIntercept implements Interceptor {
         BoundSql boundSql = statementHandler.getBoundSql();
         //select方法
         if (!"".equals(boundSql.getSql()) && boundSql.getSql().toUpperCase().trim().startsWith("SELECT")) {
+            //仅入参一个pageBean对象
             Object params = boundSql.getParameterObject();
             if (params instanceof PageBean) {
                 Connection connection = (Connection) invocation.getArgs()[0];
@@ -69,6 +71,56 @@ public class MybatisPageIntercept implements Interceptor {
                         ((pageBean.getCurrentPage() - 1) * pageBean.getPageSize()) +
                         ", " + pageBean.getPageSize();
                 metaObject.setValue("delegate.boundSql.sql", pageSql);
+            }
+
+
+            //如果有传入多个参数对象
+            MapperMethod.ParamMap paramMap = (MapperMethod.ParamMap) boundSql.getParameterObject();
+            //检索是否有PageBean参数
+            //paramMap中的参数大致为[arg1,arg2,param1,param2] arg参数可由@Param修改成相应名字
+            //TODO:或者有什么方法能够直接对pageBean对象拦截设置name:pageBean对象进来
+            for (int n = 1; n <= (paramMap.keySet().size() / 2); n++) {
+                String k = "param" + n;
+                Object o = paramMap.get(k);
+                if (o instanceof PageBean) {
+                    Connection connection = (Connection) invocation.getArgs()[0];
+                    PreparedStatement countStatement = null;
+                    ResultSet rs = null;
+                    //查询sql总记录数
+                    String countSql = "select count(0) from (" + boundSql.getSql() + ") as total";
+                    ParameterHandler parameterHandler = (ParameterHandler) metaObject.getValue("delegate.parameterHandler");
+                    PageBean pageBean = (PageBean) paramMap.get(k);
+                    try {
+                        countStatement = connection.prepareStatement(countSql);
+                        parameterHandler.setParameters(countStatement);
+                        rs = countStatement.executeQuery();
+                        if (rs.next()) {
+                            pageBean.setTotalCount(rs.getInt(1));
+                        }
+                        log.info("分页插件查询总记录数sql:[{}],总数:[{}]", countSql, rs.getInt(1));
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                        log.error("分页插件查询:[{}],mysql错误代码:[{}]", countSql, e.getErrorCode());
+                    } finally {
+                        try {
+                            if (null != countStatement) {
+                                countStatement.close();
+                            }
+                            if (null != rs) {
+                                rs.close();
+                            }
+                        } catch (SQLException sqlEx) {
+                            log.error("分页插件关闭statement以及resultSet异常[{}]", sqlEx.getErrorCode());
+                        }
+                    }
+                    //根据当前页以及大小修改成分页sql
+                    String pageSql = boundSql.getSql() + " limit " +
+                            ((pageBean.getCurrentPage() - 1) * pageBean.getPageSize()) +
+                            ", " + pageBean.getPageSize();
+                    log.info("分页插件分页查询语句:[{}]", pageSql);
+                    metaObject.setValue("delegate.boundSql.sql", pageSql);
+                    break;
+                }
             }
         }
         return invocation.proceed();
